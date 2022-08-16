662314bb97e60954dc7fb8a3e857191061b18e88
Nicolas Preiti <nicolaspreiti45@gmail.com>
2022-08-15 21:16:52 -0300

    config database

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  JWT_SECRET: process.env.JWT_SECRET,
+  DB: {
+    NAME: process.env.DB_NAME,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    HOST: process.env.DB_HOST,
+  },
+};
